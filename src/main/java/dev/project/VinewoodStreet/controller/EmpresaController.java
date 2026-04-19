3c4cde7e08f0243f17dfcfebd9679fb0fc1be431
Pedro Leal Lovatto <pedrolovatto99@gmail.com>
2026-04-19 01:11:15 -0300

    feat: adicionado controller para empresa e noticia

--- src/main/java/dev/project/VinewoodStreet/controller/EmpresaController.java
+++ src/main/java/dev/project/VinewoodStreet/controller/EmpresaController.java
@@ -1,6 +1,18 @@
 package dev.project.VinewoodStreet.controller;
 
+import dev.project.VinewoodStreet.dto.response.EmpresaDetalheDTO;
+import dev.project.VinewoodStreet.dto.response.EmpresasCardsDTO;
 import dev.project.VinewoodStreet.service.EmpresaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +22,38 @@ public class EmpresaController {
 
     EmpresaService empresaService;
 
+    public EmpresaController(EmpresaService empresaService) {
+        this.empresaService = empresaService;
+    }
+
+    @GetMapping
+    @Operation(summary = "Listar empresas na tela inicial", description = "Listar todas as empresas na tela inicial")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "lista de empresas")
+    })
+    public ResponseEntity<Page<EmpresasCardsDTO>> listarEmpresas(@PageableDefault(size = 10)Pageable paginacao) {
+        Page<EmpresasCardsDTO> empresas = empresaService.listarEmpresas(paginacao);
+        return ResponseEntity.ok(empresas);
+    }
 
 
+    @GetMapping("/{id}")
+    @Operation(summary = "Listar detalhes da empresa", description = "Encontrar empresa específica pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Empresa encontrada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Empresa não encontrada")
+    })
+    public ResponseEntity<?> empresaDetalhes(@PathVariable Long id) {
+
+        EmpresaDetalheDTO empresa = empresaService.empresaDetalhes(id);
+        if(empresa != null) {
+            return ResponseEntity.ok(empresa);
+        } else {
+            return ResponseEntity.status((HttpStatus.NOT_FOUND))
+                    .body("Empresa não encontrada");
+        }
+
+    }
 
 
 
---
+++ src/main/java/dev/project/VinewoodStreet/controller/EventoNoticiaController.java
@@ -0,0 +1,38 @@
+package dev.project.VinewoodStreet.controller;
+
+import dev.project.VinewoodStreet.dto.response.EventoNoticiaDTO;
+import dev.project.VinewoodStreet.service.EventoNoticiaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/noticias")
+public class EventoNoticiaController {
+
+    EventoNoticiaService noticiaService;
+
+    public EventoNoticiaController(EventoNoticiaService noticiaService) {
+        this.noticiaService = noticiaService;
+    }
+
+    @GetMapping
+    @Operation(summary = "Listar noticas", description = "Listar todas as noticias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "lista de noticias")
+    })
+    public ResponseEntity<Page<EventoNoticiaDTO>> listarNoticias(@PageableDefault(size = 10) Pageable paginacao) {
+        Page<EventoNoticiaDTO> noticias = noticiaService.listarNoticias(paginacao);
+        return ResponseEntity.ok(noticias);
+    }
+
+
+
+}
